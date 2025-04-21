de7e314c92573414fffd139f520558df6456557e
aeon <jerry73204@gmail.com>
2025-04-21 10:41:34 +0800

    Add a rdrive_launch package collecting launch files

---
+++ src/rdrive_launch/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'rdrive_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            [f'resource/{package_name}']),
+        (f'share/{package_name}', ['package.xml']),
+        (f'share/{package_name}/launch', ['launch/pilot.launch.yaml', 'launch/vehicle.launch.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aeon',
+    maintainer_email='jerry73204@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
