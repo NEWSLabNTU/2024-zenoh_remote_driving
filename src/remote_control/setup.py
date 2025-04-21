697c2781bacc5d8ccda566c4426bfc9c37a5f9e9
aeon <jerry73204@gmail.com>
2025-04-22 00:50:01 +0800

    fix(remote_control): add adafruit-pca9685 in install_requires

--- src/remote_control/setup.py
+++ src/remote_control/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'adafruit-pca9685'],
     zip_safe=True,
     maintainer='newslab',
     maintainer_email='newslab@todo.todo',
