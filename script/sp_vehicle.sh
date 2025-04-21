33c0b309d268fdc57e0cef17217ab8e62a6410b0
aeon <jerry73204@gmail.com>
2025-04-22 01:36:10 +0800

    Runzenoh-bridge-ros2dds directly instead of relative paths

--- script/sp_vehicle.sh
+++ script/sp_vehicle.sh
@@ -12,6 +12,6 @@ tmux send-keys -t bridge_ffmpeg "taskset --cpu-list 0-2 bash $HOME/zenoh_remote_
 # Autoware
 export ROS_DOMAIN_ID=1
 tmux new -s bridge_autoware -d
-tmux send-keys -t bridge_autoware "RUST_LOG=info ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -l tcp/${VEHICLE_IP}:8001" ENTER
+tmux send-keys -t bridge_autoware "RUST_LOG=info zenoh-bridge-ros2dds -l tcp/${VEHICLE_IP}:8001" ENTER
 tmux new -s autoware -d
 tmux send-keys -t autoware "taskset --cpu-list 3-11 ros2 launch autoware_launch planning_simulator.launch.xml map_path:=$HOME/autoware_map/test/ vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit" ENTER
--- script/vehicle.sh
+++ script/vehicle.sh
@@ -13,7 +13,7 @@ tmux send-keys -t bridge_ffmpeg "taskset --cpu-list 0-2 bash $HOME/zenoh_remote_
 
 # Autoware
 tmux new -s bridge_autoware -d
-tmux send-keys -t bridge_autoware "RUST_LOG=info ./external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds -l tcp/${VEHICLE_IP}:8001" ENTER
+tmux send-keys -t bridge_autoware "RUST_LOG=info zenoh-bridge-ros2dds -l tcp/${VEHICLE_IP}:8001" ENTER
 
 tmux new -s autoware -d
 tmux send-keys -t autoware "sleep 5 && taskset --cpu-list 3-11 ros2 launch autoware_launch autoware.launch.xml map_path:=$HOME/autoware_map/sample-map-planning vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit" ENTER
